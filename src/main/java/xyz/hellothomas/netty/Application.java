11c1b77d575c4e1a929d652169e905f08383f966
hellothomas <tyty2017@qq.com>
2021-05-23 11:17:59 +0800

    first commit

---
+++ src/main/java/xyz/hellothomas/netty/Application.java
@@ -0,0 +1,17 @@
+package xyz.hellothomas.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Thomas
+ * @date 2021/5/23 10:54
+ * @description
+ * @version 1.0
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
