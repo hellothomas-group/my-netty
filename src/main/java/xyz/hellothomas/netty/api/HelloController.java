5ade13cbe0602ba31c869a31f0621d9406afa9c0
hellothomas <tyty2017@qq.com>
2021-05-29 16:40:40 +0800

    test sync-callable

--- src/main/java/xyz/hellothomas/netty/api/HelloController.java
+++ src/main/java/xyz/hellothomas/netty/api/HelloController.java
@@ -1,7 +1,11 @@
 package xyz.hellothomas.netty.api;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import xyz.hellothomas.jedi.core.utils.SleepUtil;
+
+import java.util.concurrent.Callable;
 
 /**
  * @author Thomas
@@ -9,10 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
  * @description
  * @version 1.0
  */
+@Slf4j
 @RestController
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello(String name) {
         return "hello " + name;
+    }
+
+    @GetMapping("/sync-callable")
+    public Callable syncCallabel() {
+        return () -> {
+            log.info("{}>>>开始处理...", Thread.currentThread());
+            SleepUtil.sleep(10000);
+            log.info("{}>>>处理完成...", Thread.currentThread());
+            return "someView";
+        };
     }
 }
